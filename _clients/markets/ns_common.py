a10c1b9ad3595fdd711664523e8988514c607695
Steven <shida1@ualberta.ca>
2020-08-20 21:59:58 -0600

    Clean up market client

---
+++ _clients/markets/ns_common.py
@@ -0,0 +1,44 @@
+import socketio
+
+class NSDefault(socketio.AsyncClientNamespace):
+    def __init__(self, market):
+        super().__init__(namespace='')
+        self.market = market
+
+    # async def on_connect(self):
+    #     pass
+
+    # async def on_disconnect(self):
+        # print('disconnected from server')
+
+    # async def on_mode_switch(self, mode_data):
+        # in RT mode, market controls timing
+        # in sim mode, market gives up timing control to sim controller
+        # self.market.mode_switch(mode_data)
+        # print(mode_data)
+
+class NSSimulation(socketio.AsyncClientNamespace):
+    def __init__(self, market):
+        super().__init__(namespace='/simulation')
+        self.market = market
+
+    # async def on_connect(self):
+        # print('connected to simulation')
+        # pass
+
+    # async def on_disconnect(self):
+        # print('disconnected from simulation')
+
+
+    async def on_start_round(self, message):
+        await self.market.step(message['duration'], sim_params=message)
+
+    async def on_start_generation(self, message):
+        table_name = str(message['generation']) + '_' + message['market_id']
+        await self.market.open_db(message['db_string'], table_name)
+
+    async def on_end_generation(self, message):
+        await self.market.end_sim_generation()
+
+    async def on_end_simulation(self):
+        self.market.run = False
---
+++ _clients/markets/sio_client.py
@@ -0,0 +1,79 @@
+import os
+import asyncio
+import importlib
+import socketio
+import tenacity
+import json
+from _utils import jkson
+
+from _clients.markets.ns_common import NSDefault, NSSimulation
+
+if os.name == 'posix':
+    import uvloop
+    uvloop.install()
+
+class Client:
+    def __init__(self, server_address, market_configs):
+        # Initialize client-server data
+        self.server_address = server_address
+        self.sio_client = socketio.AsyncClient(reconnection=True,
+                                               reconnection_attempts=100,
+                                               reconnection_delay=1,
+                                               reconnection_delay_max=5,
+                                               randomization_factor=0.5,
+                                               json=jkson)
+
+        market_configs = market_configs
+        market_configs['market_id'] = market_configs.pop('id', '')
+        grid_params = market_configs.pop('grid', {})
+
+        # Initialize market information
+        Market = importlib.import_module('_clients.markets.' + market_configs['type']).Market
+        NSMarket = importlib.import_module('_clients.markets.' + market_configs['type']).NSMarket
+
+        self.market = Market(sio_client=self.sio_client,
+                             **market_configs,
+                             grid_params=grid_params)
+
+        # register client in server rooms
+        self.sio_client.register_namespace(NSDefault(self.market))
+        self.sio_client.register_namespace(NSMarket(self.market))
+        self.sio_client.register_namespace(NSSimulation(self.market))
+
+        self.data_recorded = False
+        self.recording_complete = False
+    
+    @tenacity.retry(wait=tenacity.wait_fixed(1))
+    async def start_client(self):
+        await self.sio_client.connect(self.server_address)
+        await self.sio_client.wait()
+
+    async def run(self):
+        tasks = [
+            asyncio.create_task(self.start_client()),
+            asyncio.create_task(self.market.loop())]
+        try:
+            await asyncio.gather(*tasks)
+        except SystemExit:
+            for t in tasks:
+                t.cancel()
+            raise SystemExit
+
+def __main():
+    import socket
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--host', default=socket.gethostbyname(socket.getfqdn()), help='')
+    parser.add_argument('--port', default=42069, help='')
+    parser.add_argument('--configs')
+    args = parser.parse_args()
+
+    client = Client(server_address=''.join(['http://', args.host, ':', str(args.port)]),
+                    market_configs=json.loads(args.configs))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(client.run())
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(__main())
