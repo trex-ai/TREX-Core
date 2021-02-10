987abe1635bc85a26a3bcfa55d860db425681cf8
Steven <shida1@ualberta.ca>
2021-02-09 19:32:55 -0700

    make rewards into modules

--- _agent/rewards/unit_profit_and_cost.py
+++ _agent/rewards/unit_profit_and_cost.py
@@ -1,13 +1,15 @@
-from _agent._rewards.utils import process_ledger
+from _agent.rewards.utils import process_ledger
 
 class Reward:
+    # Calculates unit profit and cost
+    # needed for sma_crossover_trader
     def __init__(self, timing=None, ledger=None, market_info=None, **kwargs):
         self.__timing = timing
         self.__ledger = ledger
         self.__market_info = market_info
-        # self.last = {}
 
-    async def calculate(self, last_deliver = None, market_transactions=None, grid_transactions=None, financial_transactions=None):
+    async def calculate(self, last_deliver=None, market_transactions=None, grid_transactions=None,
+                        financial_transactions=None):
         """
         Parameters:
             dict : settlements
@@ -19,7 +21,7 @@ class Reward:
             else:
                 last_deliver = self.__timing['last_deliver']
 
-        if (market_transactions==None and grid_transactions==None and financial_transactions==None):
+        if (market_transactions == None and grid_transactions == None and financial_transactions == None):
             market_transactions, grid_transactions, financial_transactions = \
                 await process_ledger(last_deliver, self.__ledger, self.__market_info)
 
@@ -31,16 +33,10 @@ class Reward:
         grid_sell_price = grid_transactions[3]
         grid_buy_price = grid_transactions[1]
 
-        nme_profit = grid_sell_price * asks_qty
-        market_advantage_profit = market_profit - nme_profit
+        unit_profit = market_profit / asks_qty if asks_qty > 0 else grid_sell_price
+        unit_cost = market_cost / bids_qty if bids_qty > 0 else grid_buy_price
 
-        nme_cost = grid_buy_price * bids_qty
-        market_advantage_cost = market_cost - nme_cost
+        unit_profit_diff = unit_profit - grid_sell_price
+        unit_cost_diff = unit_cost - grid_buy_price
 
-        financial_costs = financial_transactions[0] if financial_transactions else 0
-        financial_profit = financial_transactions[1] if financial_transactions else 0
-
-        profit_diff =  market_advantage_profit + financial_profit
-        cost_diff =  market_advantage_cost + financial_costs
-        reward = float(profit_diff - cost_diff)/1000 #divide by 1000 because units so far are $/kWh * wh
-        return reward
+        return [unit_profit, unit_profit_diff, unit_cost, unit_cost_diff]
