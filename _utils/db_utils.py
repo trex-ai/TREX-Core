ba6b7d77b93d1a3308effa9604a0658e46a3fdd6
Steven <shida1@ualberta.ca>
2022-01-31 12:30:34 -0700

    add max connections to profile db connection parameters

--- _utils/db_utils.py
+++ _utils/db_utils.py
@@ -1,7 +1,7 @@
 # https://stackoverflow.com/questions/30778015/how-to-increase-the-max-connections-in-postgres
 
 import sqlalchemy
-from sqlalchemy import create_engine, MetaData, Column
+from sqlalchemy import create_engine, MetaData, Column, func
 from sqlalchemy_utils import database_exists, create_database
 import databases
 
@@ -12,12 +12,14 @@ def create_db(db_string):
     return database_exists(engine.url)
 
 async def dump_data(data, db_string, table):
-    db = databases.Database(db_string)
-    await db.connect()
-    query = table.insert()
-    async with db.transaction():
-        await db.execute_many(query, data)
-    await db.disconnect()
+    # db = databases.Database(db_string)
+    # await db.connect()
+    # query = table.insert()
+    async with databases.Database(db_string) as db:
+        async with db.transaction():
+            query = table.insert()
+            await db.execute_many(query, data)
+    # await db.disconnect()
 
 def get_table(db_string, table_name, engine=None):
     if not engine:
@@ -32,7 +34,10 @@ def get_table(db_string, table_name, engine=None):
 
 def get_table_len(db_string, table):
     engine = create_engine(db_string)
-    return engine.scalar(table.count())
+    with engine.begin() as connection:
+        rows = connection.execute(func.count(table.id)).scalar()
+    return rows
+    # return engine.scalar(table.count())
 
 def drop_table(db_string, table_name):
     engine = create_engine(db_string)
