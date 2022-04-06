643647f6874891bd97b8da7567ac6ba76844cf11
Steven <shida1@ualberta.ca>
2022-04-05 19:41:47 -0600

    !!!BREAKING CHANGE: SIMULATIONS ARE DEFINED BY A SET OF SIMULATION TYPES

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ if __name__ == '__main__':
     # file name must be config name. Case sensitive.
     # it is sometimes possible to resume a simulation in case it crashes in the middle
     # however due to the complexity this is experimental and cannot be relied upon
-    runner = Runner(config='config_name', resume=False, purge=False)
+    runner = Runner(config='config_name', resume=False, purge=True)
 
-    # list of simulations to be performed.
+    # set of simulation types to be performed
     # in general, it is recommended to perform at baseline and training at minimum
-    simulations = [
-        {'simulation_type': 'baseline'},
-        # {'simulation_type': 'training'},
-        # {'simulation_type': 'validation'}
-    ]
+    simulations = {
+        # 'baseline',
+        'training'
+        # 'validation'
+    }
+
     runner.run(simulations)
