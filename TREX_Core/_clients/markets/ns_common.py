c659e64e2016ce42f7ffcb3d0e232ca3bee59cc0
Steven <steven@trex-ai.ca>
2024-01-31 17:38:43 -0800

    tag settlement delivered msg with participant id

--- TREX_Core/_clients/markets/ns_common.py
+++ TREX_Core/_clients/markets/ns_common.py
@@ -104,8 +104,9 @@ class NSDefault():
         ask = json.loads(ask)
         return await self.market.submit_ask(ask)
 
-    async def on_settlement_delivered(self, commit_id):
-        await self.market.settlement_delivered(commit_id)
+    async def on_settlement_delivered(self, message):
+        message = json.loads(message)
+        await self.market.settlement_delivered(message)
 
     async def on_meter_data(self, message):
         # print("meter data")
