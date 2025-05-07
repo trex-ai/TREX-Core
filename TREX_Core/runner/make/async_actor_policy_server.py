28c258001b79e719e8fc1c314c81e2f0a9779888
Daniel May <dcmay@ualberta.ca>
2025-05-06 20:42:59 -0600

    Added maker clis for synchronous and asynchronous policy servers

---
+++ TREX_Core/runner/make/async_actor_policy_server.py
@@ -0,0 +1,30 @@
+import json
+import os
+# from pathlib import
+
+# this is the makefile for an asynchronous actor policy server,
+
+# Fixme: the name that is used in the makefile here does not read from the config file?
+def cli(configs):
+    path = __file__.split('runner')
+    script_path = os.path.join(path[0], 'appo', 'async_actor.py')
+    if not os.path.exists(script_path):
+        path = os.getcwd()
+        script_path = os.path.join(path, 'clients', 'appo', 'async_actor.py')
+    # print('policy server', script_path)
+    if not os.path.exists(script_path):
+        return None, None
+
+    if 'server' not in configs:
+        return None, None
+
+    host = configs['server']['host']
+    port = str(configs['server']['port'])
+
+    args = []
+    if host:
+        args.append('--host=' + host)
+    if port:
+        args.append('--port=' + port)
+    args.append('--config=' + json.dumps(configs))
+    return (script_path, args)
---
+++ TREX_Core/runner/make/async_learner_policy_server.py
@@ -0,0 +1,32 @@
+import json
+import os
+# from pathlib import
+
+# this is the makefile for a synchronous policy server,
+# where the collection of experience and the learning on that experience happen in sequence and not in parallel
+# as such, only one client is needed
+
+# Fixme: the name that is used in the makefile here does not read from the config file?
+def cli(configs):
+    path = __file__.split('runner')
+    script_path = os.path.join(path[0], 'appo', 'async_learner.py')
+    if not os.path.exists(script_path):
+        path = os.getcwd()
+        script_path = os.path.join(path, 'clients', 'appo', 'async_learner.py')
+    # print('policy server', script_path)
+    if not os.path.exists(script_path):
+        return None, None
+
+    if 'server' not in configs:
+        return None, None
+
+    host = configs['server']['host']
+    port = str(configs['server']['port'])
+
+    args = []
+    if host:
+        args.append('--host=' + host)
+    if port:
+        args.append('--port=' + port)
+    args.append('--config=' + json.dumps(configs))
+    return (script_path, args)
