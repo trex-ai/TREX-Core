eefbc0a96190b45708117c5cd3dc851dc1b6b1c6
Mikael Sabuhi <mikael.sabuhi@gmail.com>
2026-03-12 14:41:39 -0600

    wip: added sample logging in main added pytz stubs.

--- TREX_Core/main.py
+++ TREX_Core/main.py
@@ -1,18 +1,28 @@
-if __name__ == '__main__':
+import structlog
+
+from TREX_Core.logging_config import setup_logging
+
+setup_logging(json_output=False)  # JSON in prod, False for local dev
+log = structlog.get_logger()
+if __name__ == "__main__":
     from TREX_Core.runner import Runner
+
     # configuration to be used must be under configs
     # file name must be config name. Case sensitive.
     # it is sometimes possible to resume a simulation in case it crashes in the middle
     # however due to the complexity this is experimental and cannot be relied upon
-    runner = Runner(config='citylearn_test', purge=True)
+    config_name = "citylearn_test"
+    log.info("Starting simulation!", config_name=config_name)
+    runner = Runner(config=config_name, purge=True)
 
     # list of simulations to be performed.
     # in general, it is recommended to perform at base line and training at minimum
     simulations = {
-        'baseline'
+        "baseline"
         # 'training'
         # 'validation'
     }
+    log.info("Running simulations", simulations=simulations)
     runner.run_simulations(simulations)
     # launch_list = runner.make_launch_list()
     # runner.run([launch_list[3]])
